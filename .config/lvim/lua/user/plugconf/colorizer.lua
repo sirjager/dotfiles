4bea0ec1d65bef7ed7f5c8ed6a3d784ad364edf7
Ankur Kumar <kankurcool@gmail.com>
2024-03-04 13:02:59 +0530

    feat: hyprland setup from prasanthrangan/hyprdots

--- .config/lvim/lua/user/plugconf/colorizer.lua
+++
@@ -1,8 +0,0 @@
-local name = "colorizer"
-local ok, pkg = pcall(require, name)
-if not ok then
-	vim.notify({ pkg .. " is not installed" }, "warn")
-	return
-end
-
-pkg.setup({ "*" })
--- .config/lvim/lua/user/plugconf/init.lua
+++
@@ -1,5 +0,0 @@
-reload("user.plugconf.toggleterm")
-reload("user.plugconf.neoscroll")
-reload("user.plugconf.colorizer")
-
-
